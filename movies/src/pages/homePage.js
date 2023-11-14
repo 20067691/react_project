3338f46f23ac42a7a6cdaf6dd1ee89696c1a7bda
DeanS <20067691@mail.wit.ie>
2023-11-14 21:17:39 +0000

    Ppular actor page, start on pagination

--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -5,28 +5,28 @@ import { useQuery } from 'react-query';
 import Spinner from '../components/spinner';
 import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
 import { getTopRatedMovie } from "../api/tmdb-api";
-import { getPopularPeople } from "../api/tmdb-api";
+
 import TopRatedMovieList from "../components/topRatedList";
-import ActorList from "../components/actorList";
+
 const HomePage = (props) => {
 
   const {  data, error, isLoading, isError }  = useQuery('discover', getMovies)
   const {  data: topMovie, error: topMovieError, isLoading: topMovieLoading, isError: topMovieIsError }  = useQuery('topRated', getTopRatedMovie)
-  const {  data: people, error: peopeleError, isLoading: peopleLoading, isError: peopleError }  = useQuery('popular-people', getPopularPeople)
+  
   
 
-  if (isLoading || topMovieLoading || peopleLoading) {
+  if (isLoading || topMovieLoading) {
     return <Spinner />
   }
 
-  if (isError || topMovieIsError || peopleError) {
+  if (isError || topMovieIsError) {
     return <h1>{error.message || topMovieError.message}</h1>
   }  
   const movies = data.results;
   const topRatedMovies = topMovie?.results?.slice(0, 3) || [];
-  const popularPeople = people.results;
+  
   console.log(topRatedMovies);
-  console.log(popularPeople);
+  
 
   
 
@@ -36,8 +36,7 @@ const HomePage = (props) => {
 
   return (
     <>
-    <ActorList actors={popularPeople} />
-    
+  
     <TopRatedMovieList movies={topRatedMovies} />
       
     <PageTemplate
